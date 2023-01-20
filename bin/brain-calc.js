ebdc6fbdf259dff513639aff450df32f9cddb4c1
Augmar <krimzik@gmail.com>
2023-01-20 19:30:59 +0300

    add brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import { sign } from 'crypto';
 import { getName, greetings, getAnswer, isAnswerCorrect, winMessage, loseMessage } from './communication.js';
+import { getRandomInt, getRandomSign } from './random.js';
 
 let phrase="Welcome to the Brain Games!";
 console.log(phrase);
@@ -9,3 +11,38 @@ const name = getName();
 
 greetings(name);
 
+console.log('What is the result of the expression?');
+let count = 0;
+
+const getRightAnswer = (a, b, sign) => {
+    if (sign === '+') {
+        return a + b;
+    } else if (sign === '-') {
+        return a - b;
+    } else if (sign === '*') {
+        return a * b;
+    } else {
+        console.log(`Should never work ${sign}`)
+        return sign;
+    }
+}
+
+while (true) {
+    let a = getRandomInt(1, 100);
+    let b = getRandomInt(1, 100);
+    let sign = getRandomSign(['-','+','*']);
+    let rightAnswer = getRightAnswer(a, b, sign);
+    console.log(`Question: ${a} ${sign} ${b}`)
+    let answer = getAnswer();
+    if (isAnswerCorrect(answer, rightAnswer)) {
+        console.log('Correct!');
+        count++;
+        if (count === 3) {
+            winMessage(name);
+            break;
+        }
+    } else {
+        loseMessage(name, answer, rightAnswer);
+        break;
+    }
+}
